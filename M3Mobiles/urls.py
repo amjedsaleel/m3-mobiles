3f33c9e0c74f4980da35203ad6193d5214f6ddcd
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-10 11:50:42 +0530

    Navbar set and template integrated

--- M3Mobiles/urls.py
+++ M3Mobiles/urls.py
@@ -10,8 +10,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the includes() function: from django.urls import includes, path
+    2. Add a URL to urlpatterns:  path('blog/', includes('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
