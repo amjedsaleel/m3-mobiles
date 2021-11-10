e766b88ef584d760418c2fab9f311f15f0735dfb
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-10 20:36:10 +0530

    Admin user dashboard

--- adminPanel/views.py
+++ adminPanel/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
 def dashboard(request):
-    return HttpResponse("dashboard")
+    return render(request, 'adminPanel/dashboard.html')
