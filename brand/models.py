bafed32efdb31eb8d7a1bf422a1e44c4e61baaed
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-11 10:05:23 +0530

    Logo added to brand

--- brand/models.py
+++ brand/models.py
@@ -10,6 +10,7 @@ from django.db import models
 class Brand(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
+    log = models.ImageField(upload_to='brands')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
