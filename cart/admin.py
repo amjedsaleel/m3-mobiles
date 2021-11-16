83586826b91abbc30856e02ca4b70f1e9324af15
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-16 10:29:26 +0530

    Cart model

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,10 @@
+# Django
 from django.contrib import admin
 
+# local Django
+from .models import Cart, CartItem
+
 # Register your models here.
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,26 @@
+# Django
 from django.db import models
 
+# local Django
+from store.models import Variant
+
 # Create your models here.
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.cart} + {self.variant}'
+
