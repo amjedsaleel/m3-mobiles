407bebf17f8e47673a26804355e701b39ad134a6
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-24 20:08:46 +0530

    COD

--- payment/urls.py
+++ payment/urls.py
@@ -9,6 +9,7 @@ app_name = 'payments'
 urlpatterns = [
     path('paypal/', views.paypal, name='paypal'),
     path('razorpay-payment-verification/', views.razorpay_payment_verification, name='razorpay-payment-verification'),
-    path('failed/', views.failed, name='payment-failed')
+    path('failed/', views.failed, name='payment-failed'),
+    path('cash-on-delivery-confirmation/', views.cod_confirmation, name='cod-confirmation'),
+    path('cash-on-delivery/', views.cash_on_delivery, name='cash-on-delivery'),
 ]
-
--- payment/views.py
+++ payment/views.py
@@ -1,14 +1,21 @@
+# Stated library
+import threading
+from datetime import datetime
+
 # third party
 import razorpay
 
 # Django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.conf import settings
+from django.contrib import messages
 
 # local Django
 from .models import Payment
+from accounts.verification import send_otp, verify_otp_number
 from order.utils import make_order
+
 
 # Create your views here.
 
@@ -66,3 +73,34 @@ def razorpay_payment_verification(request):
 
 def failed(request):
     return render(request, 'payment/payment-failed.html')
+
+
+def cod_confirmation(request):
+    t1 = threading.Thread(target=send_otp, args=(request.user.mobile, ))
+    t1.start()
+    messages.success(request, 'Enter otp to complete order')
+    return redirect('payments:cash-on-delivery')
+
+
+def cash_on_delivery(request):
+
+    if request.method == 'POST':
+        otp = request.POST.get('otp')
+        verify = verify_otp_number(request.user.mobile, otp)
+
+        if verify:
+            # Saving payment details
+            payment = Payment.objects.create(
+                user=request.user,
+                payment_id=f'COD{datetime.now().strftime("%Y%m%d%H%M%S")}',
+                amount_paid=request.session['grand_total'],
+                payment_method='COD',
+                status=True
+            )
+            request.session['payment_id'] = payment.id
+            make_order(request)
+            return redirect('order:order-completed')
+
+        messages.error(request, 'Invalid OTP')
+        return redirect('payments:cash-on-delivery')
+    return render(request, 'accounts/verify-otp.html')
