0f3541db4bef589f51a7638c09ec6bcdb6d8c9d4
Amjed Saleel K <amjedsaleel@gmail.com>
2021-11-29 10:34:43 +0530

    coupon model

--- offer/admin.py
+++ offer/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import BrandOffer, ProductOffer, VariantOffer
+from .models import BrandOffer, ProductOffer, VariantOffer, Coupon, RedeemedCoupon
 
 # Register your models here.
 
 admin.site.register(BrandOffer)
 admin.site.register(ProductOffer)
 admin.site.register(VariantOffer)
+admin.site.register(Coupon)
+admin.site.register(RedeemedCoupon)
--- offer/models.py
+++ offer/models.py
@@ -3,6 +3,7 @@ import uuid
 
 # Django
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # local Django
 from brand.models import Brand
@@ -42,3 +43,26 @@ class VariantOffer(models.Model):
     def get_variant(self):
         variant = self.variant.slug
         return variant.replace("-", " ")
+
+
+class Coupon(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    coupon_name = models.CharField(max_length=50)
+    coupon_code = models.CharField(max_length=50, unique=True)
+    discount = models.PositiveIntegerField(help_text="Offer in percentage", null=True)
+    limit = models.PositiveIntegerField()
+    used = models.PositiveIntegerField(default=0, editable=False)
+    valid_from = models.DateField()
+    valid_to = models.DateField()
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.coupon_name
+
+
+class RedeemedCoupon(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
+
+
