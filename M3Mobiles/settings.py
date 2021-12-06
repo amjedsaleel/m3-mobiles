12888216329e7e791ef9690cc140eec4aa3080d9
Amjed Saleel K <amjedsaleel@gmail.com>
2021-12-06 12:23:36 +0530

    ALLOWED_HOSTS added to .env

--- M3Mobiles/settings.py
+++ M3Mobiles/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 import os
 from django.contrib.messages import constants as messages
 
@@ -29,8 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
