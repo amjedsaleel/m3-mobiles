9e7bbeb41f05023b2a6dc4ad5345b8a2158b1be2
amjedsaleel <amjedsaleel@gmail.com>
2022-01-12 17:14:11 +0530

    Revert "Revert "digitalocean spaces""
    This reverts commit 51b5df8f5e0d0d1e7ba967770eaab8ea36dddb06.

--- M3Mobiles/settings.py
+++ M3Mobiles/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
     # Installed via pip
     'crispy_forms',
+    'storages',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
@@ -148,13 +149,25 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = config('AWS_LOCATION')
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -168,6 +181,7 @@ MESSAGE_TAGS = {
 }
 
 LOGIN_URL = '/accounts/login'
+
 
 # Twilio API credentials
 # (find here https://www.twilio.com/console)
